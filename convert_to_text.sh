648d06af8a322febb38aa68b7b2a510d5f521df1
Roman Babenko <r.babenko@samsung.com>
2018-11-20 09:16:40 +0200

    convert to text with w3m

--- convert_to_text.sh
+++ convert_to_text.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-for file in $(find html -regextype posix-extended -regex '.*\.html'); do
+for file in $(find html -regextype posix-extended -regex '.*\.html' | sort -r); do
 echo "converting $file"
 
 # convert to text
-    html2text -utf8 -style pretty -o "${file%.html}.txt" -width 65536 "$file"
+#	html2text -utf8 -style pretty -o "${file%.html}.txt" -width 65536 "$file"
+	w3m -I UTF-8 -O UTF-8 -cols 65536 "$file" > "${file%.html}.txt"
 
     sed -i 's/&mdash;/-/g' "${file%.html}.txt"
 
+	sed -i 's/━/-/g' "${file%.html}.txt"
+
     sed -i 's/---*/--------------------------------------------------------------------------------/g' "${file%.html}.txt"
 
-	sed -i 's/{.*}//g' "${file%.html}.txt"
+#	sed -i 's/{.*}//g' "${file%.html}.txt"
 
 	gawk -i inplace -v RS="" '{sub(/\{.*\}/,"")}1' "${file%.html}.txt"
 
