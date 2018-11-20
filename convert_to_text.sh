1f5cfd7a98adb4d4851514cfa33704409dbcc00d
Roman Babenko <r.babenko@samsung.com>
2018-11-20 08:09:14 +0200

    converted to text

---
+++ convert_to_text.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+for file in `find html -regextype posix-extended -regex '.*\.html'`; do
+echo "converting $file"
+
+# convert to text
+    html2text -utf8 -style pretty -o "${file%.html}.txt" -width 65536 "$file"
+
+    sed -i 's/&mdash;/-/g' "${file%.html}.txt"
+
+# replace nonbreaking space for UTF
+    sed -i 's/\xC2\xA0/ /g' "${file%.html}.txt"
+
+# remove leading and ending whitespace
+    sed -i 's/^ *//g' "${file%.html}.txt"
+    sed -i 's/ *$//g' "${file%.html}.txt"
+
+# remove double spaces
+    sed -i 's/  */ /g' "${file%.html}.txt"
+
+# extra line end conversion
+    cat "${file%.html}.txt" | tr '\n' '#' > "$file.utf-8"
+
+    mv "$file.utf-8" "${file%.html}.txt"
+
+    sed -i 's/##*/#/g' "${file%.html}.txt"
+    sed -i 's/#/\n\n/g' "${file%.html}.txt"
+
+echo "$file done"
+done
+
+mv ./html/*.txt ./text
